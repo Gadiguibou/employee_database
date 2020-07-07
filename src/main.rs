b598e7e2dba942935dd5889053d791e7b06c0b8a
Gabriel Lacroix <lacroixgabriel@gmail.com>
2020-07-07 11:20:40 -0400

    Add a Remove <> from <> Command

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,20 @@ fn main() {
         let input = input.trim();
 
         match Command::from_input(input) {
-            Some(Command::Add { name, department }) => employees
-                .entry(department)
-                .or_default()
-                .push(name),
+            Some(Command::Add { name, department }) => {
+                employees.entry(department).or_default().push(name)
+            }
+            Some(Command::Remove { name, department }) => match employees.get_mut(&department) {
+                Some(names) => {
+                    for i in 0..names.len() {
+                        if names[i] == name {
+                            names.swap_remove(i);
+                            break; // How could I print a statement if no employees were found???
+                        }
+                    }
+                }
+                None => println!("No department found with this name."),
+            },
             Some(Command::List(department)) => println!("{:?}", employees.entry(department)),
             Some(Command::ListAll) => {
                 for department in &employees {
@@ -34,6 +44,7 @@ fn main() {
 
 enum Command {
     Add { name: String, department: String },
+    Remove { name: String, department: String },
     List(String),
     ListAll,
     Quit,
@@ -45,6 +56,10 @@ impl Command {
 
         match split.as_slice() {
             ["Add", name, "to", department] => Some(Command::Add {
+                name: name.to_string(),
+                department: department.to_string(),
+            }),
+            ["Remove", name, "from", department] => Some(Command::Remove {
                 name: name.to_string(),
                 department: department.to_string(),
             }),
